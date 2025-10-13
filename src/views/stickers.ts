06f5f020460acccc3e658f744d373c4fa1af3f34
akrck02 <aketza.vazquez@gmail.com>
2025-10-13 18:27:02 +0200

    sticker logic

--- src/views/stickers.ts
+++ src/views/stickers.ts
@@ -1,63 +1,104 @@
-import { BubbleUI } from '../lib/bubble.js';
-import { setDomEvents, setDomStyles, uiComponent } from '../lib/dom.js';
-import { Html } from '../lib/html.js';
+import { BubbleUI } from "../lib/bubble.js";
+import { setDomEvents, setDomStyles, uiComponent } from "../lib/dom.js";
+import { Html } from "../lib/html.js";
 
 let stickers = {
-  Bojack: 'Bojacksticker.png',
-  Cuervo: 'cuervo.png',
-  'Da funk': 'Dafunkyouwant.png',
-  Desdentao: 'Desdentaosticker.png',
-  'Cat souls': 'Dscat.png',
-  'Focas 1': 'Focassticker1.png',
-  'Focas 2': 'Focassticker2.png',
-  Frieren: 'Frierensticker.png',
-  'Meowdalenas 1': 'Gatosmuffin1.png',
-  'Meowdalenas 2': 'Gatosmuffin2.png',
-  'Meowdalenas 3': 'Gatosmuffin3.png',
-  'Meowdalenas 4': 'Gatosmuffin4.png',
-  Gelp: 'Glepsticker.png',
-  Sardiboy: 'sardiboy.png'
+	Bojack: "Bojacksticker.png",
+	Cuervo: "cuervo.png",
+	"Da funk": "Dafunkyouwant.png",
+	Desdentao: "Desdentaosticker.png",
+	"Cat souls": "Dscat.png",
+	"Focas 1": "Focassticker1.png",
+	"Focas 2": "Focassticker2.png",
+	Frieren: "Frierensticker.png",
+	"Meowdalenas 1": "Gatosmuffin1.png",
+	"Meowdalenas 2": "Gatosmuffin2.png",
+	"Meowdalenas 3": "Gatosmuffin3.png",
+	"Meowdalenas 4": "Gatosmuffin4.png",
+	Gelp: "Glepsticker.png",
+	Sardiboy: "sardiboy.png",
 };
 
-let currentSticker = 'Gelp';
+let currentStickerId = "Gelp";
+let selected = false;
 
-export async function showStickerView(parameters: string[], container: HTMLElement) {
-  const view = uiComponent({
-    type: Html.View,
-    id: 'home',
-    classes: [BubbleUI.BoxRow, BubbleUI.BoxXStart, BubbleUI.BoxYStart],
-    styles: {
-      width: '100%',
-      height: '100%',
-      transition: 'top 5s, left 5s'
-    }
-  });
+let currentSticker;
 
-  let sticker = uiComponent({
-    type: Html.Img,
-    attributes: {
-      src: `resources/images/stickers/${stickers[currentSticker]}`
-    },
-    styles: {
-      position: 'fixed',
-      width: '10rem',
-      transform: 'scale(2)',
-      filter: 'drop-shadow(.2rem .2rem rgba(0,0,0,0.15))'
-    }
-  });
+export async function showStickerView(
+	parameters: string[],
+	container: HTMLElement,
+) {
+	const view = uiComponent({
+		type: Html.View,
+		id: "home",
+		classes: [BubbleUI.BoxRow, BubbleUI.BoxXStart, BubbleUI.BoxYStart],
+		styles: {
+			width: "100%",
+			height: "100%",
+			transition: "top 5s, left 5s",
+		},
+	});
 
-  setDomEvents(view, {
-    mousemove: (e: MouseEvent) => {
-      let x = e.clientX;
-      let y = e.clientY;
-      setDomStyles(sticker, {
-        top: `calc(${y}px - ${sticker.offsetHeight / 2}px)`,
-        left: `calc(${x}px - ${sticker.offsetWidth / 2}px)`
-      });
-    }
-  });
+	setDomEvents(view, {
+		mousemove: (e: MouseEvent) => {
+			if (!selected || currentSticker == undefined) return;
+			let x = e.clientX;
+			let y = e.clientY;
+			setDomStyles(currentSticker, {
+				top: `calc(${y}px - ${currentSticker.offsetHeight / 2}px)`,
+				left: `calc(${x}px - ${currentSticker.offsetWidth / 2}px)`,
+			});
+		},
+	});
 
-  view.appendChild(sticker);
+	let selector = uiComponent({
+		type: Html.Img,
+		attributes: {
+			src: `resources/images/stickers/${stickers[currentStickerId]}`,
+		},
+		styles: {
+			position: "fixed",
+			width: "10rem",
+			transform: "scale(1)",
+			transition: "transform .25s",
+			filter: "drop-shadow(.2rem .2rem rgba(0,0,0,0.15))",
+		},
+	});
 
-  container.appendChild(view);
+	setDomStyles(selector, {
+		top: `calc(50% - ${selector.offsetHeight / 2}px)`,
+		left: `calc(50% - ${selector.offsetWidth / 2}px)`,
+	});
+
+	selector.onclick = () => {
+		currentSticker = createSticker();
+		view.appendChild(currentSticker);
+
+		currentSticker.onclick = () => {
+			selected = !selected;
+			setDomStyles(currentSticker, {
+				transform: selected ? "scale(1.2)" : "scale(1)",
+			});
+		};
+	};
+
+	view.appendChild(selector);
+	container.appendChild(view);
+}
+
+function createSticker(): HTMLElement {
+	let sticker = uiComponent({
+		type: Html.Img,
+		attributes: {
+			src: `resources/images/stickers/${stickers[currentStickerId]}`,
+		},
+		styles: {
+			position: "fixed",
+			width: "10rem",
+			transform: "scale(2)",
+			filter: "drop-shadow(.2rem .2rem rgba(0,0,0,0.15))",
+		},
+	});
+
+	return sticker;
 }
