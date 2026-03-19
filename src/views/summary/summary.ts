a1aa0e9c7d7551d1dcd3cefbf52a3be5e46aa718
aketza <aketza.eguskiza@kaytek.io>
2026-03-19 22:43:01 +0100

    etsy merch!

--- src/views/summary/summary.ts
+++ src/views/summary/summary.ts
@@ -1,77 +1,92 @@
-import { BubbleUI } from '../../lib/bubble.js';
-import { getConfiguration } from '../../lib/configuration.js';
-import { uiComponent } from '../../lib/dom.js';
-import { Html } from '../../lib/html.js';
-import { httpGet } from '../../lib/http.js';
+import { BubbleUI } from "../../lib/bubble.js";
+import { getConfiguration } from "../../lib/configuration.js";
+import { uiComponent } from "../../lib/dom.js";
+import { Html } from "../../lib/html.js";
+import { httpGet } from "../../lib/http.js";
+import Icons from "../../services/icons.js";
 
-export async function showSummaryView(
-  parameters: string[],
-  container: HTMLElement
-) {
+export async function showSummaryView(_: string[], container: HTMLElement) {
   const view = uiComponent({
     type: Html.View,
-    id: 'summary',
-    classes: [BubbleUI.BoxColumn, BubbleUI.BoxCenter],
-    styles: {
-      marginTop: '10rem'
-    }
+    id: "summary",
+    classes: [BubbleUI.BoxColumn, BubbleUI.BoxCenter]
   });
+  container.appendChild(view);
 
   const title = uiComponent({
     type: Html.Img,
+    id: "logo",
     attributes: {
-      src: `${getConfiguration('path')['images']}/skyleriearts-logo.png`
-    },
-    styles: {
-      width: '20rem'
+      src: `${getConfiguration("path")["images"]}/skyleriearts-logo.png`
     }
   });
   view.appendChild(title);
 
   const comment = uiComponent({
     type: Html.Text,
-    text: 'We are rebuilding this website!',
-    styles: {
-      marginTop: '2rem',
-      color: '#A870C3',
-      fontSize: '1rem'
-    }
+    id: "subtitle",
+    text: "We are rebuilding this website!"
   });
   view.appendChild(comment);
 
-  const description = uiComponent({
-    type: Html.Text,
-    text: 'Contact me',
-    styles: {
-      marginTop: '5rem',
-      color: '#A870C3',
-      fontSize: '2rem',
-      textDecoration: 'underline'
-    }
+  const merchBanner = uiComponent({
+    id: "merch-banner",
+    classes: [BubbleUI.BoxRow, BubbleUI.BoxCenter]
   });
-  view.appendChild(description);
+  view.appendChild(merchBanner);
+
+  merchBanner.onclick = () => {
+    window.open("https://skyleriearts.etsy.com", "_blank");
+  };
+
+  const merchTextColumn = uiComponent({});
+  merchBanner.appendChild(merchTextColumn);
+
+  const merchText = uiComponent({
+    type: Html.P,
+    id: "merch-text",
+    text: "New merch available!"
+  });
+  merchTextColumn.appendChild(merchText);
+
+  const merchText2 = uiComponent({
+    type: Html.P,
+    id: "merch-text-2",
+    text: "Click here to visit the store."
+  });
+  merchTextColumn.appendChild(merchText2);
+
+  const merchIcon = uiComponent({
+    type: Html.Div
+  });
+  merchBanner.appendChild(merchIcon);
 
   const linksResponse = await httpGet({
-    url: `${getConfiguration('path')['data']}/social.json`,
+    url: `${getConfiguration("path")["data"]}/social.json`,
     parameters: {}
   });
 
   const links = await linksResponse.json();
+  const linkContainer = uiComponent({
+    classes: [BubbleUI.BoxRow]
+  });
 
   for (const name in links) {
     const socialButton = uiComponent({
       type: Html.A,
       text: name,
+      classes: ["social-button"],
+
       attributes: {
         href: links[name],
-        target: '_blank'
-      },
-      styles: {
-        marginTop: '1rem',
-        fontSize: '1.5rem'
+        target: "_blank"
       }
     });
-    view.appendChild(socialButton);
+    linkContainer.appendChild(socialButton);
+    Icons.load(`/resources/icons/${name}.svg`, (content) => (socialButton.innerHTML = content));
   }
-  container.appendChild(view);
+
+  view.appendChild(linkContainer);
+
+  Icons.load("/resources/icons/shopping_bag.svg", (content) => (merchIcon.innerHTML = content));
 }
